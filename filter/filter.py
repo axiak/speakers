c2690d6af10f596878e2ac68ebbe10a52faf1822
Mike Axiak <mike@axiak.net>
2016-06-11 00:57:09 -0400

    Add filter cache and filter factory

---
+++ filter/filter.py
@@ -0,0 +1,156 @@
+import os
+import numpy
+import shelve
+import pickle
+import scipy.signal
+from functools import wraps
+from matplotlib import pyplot
+
+
+class Filter(object):
+    sample_freq = None
+    coefficients = None
+    name = None
+    filter_type = None
+
+    def __init__(self, filter_type, name, sample_freq, coefficients):
+        self.filter_type = filter_type
+        self.name = name
+        self.sample_freq = sample_freq
+        self.coefficients = coefficients
+
+    def plot_filter(self, output_file=None, prompt=True):
+        filter_coefs = self.coefficients
+        t = numpy.linspace(0, float(len(filter_coefs - 1)) / self.sample_freq, len(filter_coefs))
+        fig = pyplot.figure(figsize=(10, 10))
+
+        pyplot.subplot(211)
+
+        pyplot.plot(t, numpy.abs(filter_coefs))
+        pyplot.plot(t, numpy.real(filter_coefs))
+        pyplot.legend(['abs', 'real'], loc='upper right')
+
+        pyplot.grid(True)
+        pyplot.xlabel('Time (s)')
+        pyplot.ylabel('Filter coefficient')
+        pyplot.title('Filter impulse response')
+
+        ax1 = fig.add_subplot(212)
+        (freq, ampl) = scipy.signal.freqz(self.coefficients)
+        freq_hz = (freq * self.sample_freq / (2 * numpy.pi))[1:]
+
+        pyplot.semilogx(freq_hz, 20 * numpy.log10(numpy.abs(ampl[1:])), 'b-')
+        pyplot.grid(True)
+        pyplot.ylabel('Amplitude (dB)')
+        pyplot.legend(['Amplitude', 'Phase'], loc='upper right')
+
+        ax1.twinx()
+        pyplot.semilogx(freq_hz, 180 / numpy.pi * numpy.unwrap(numpy.angle(ampl[1:])), 'r-')
+        pyplot.ylabel('Phase angle (deg)')
+        pyplot.xlabel('Frequency (Hz)')
+        pyplot.legend(['Phase'], loc='lower right')
+        pyplot.title('Filter frequency response')
+
+        if output_file:
+            pyplot.savefig(output_file)
+        else:
+            if prompt:
+                print 'Please close figure window to continue'
+            pyplot.show()
+
+    def __repr__(self):
+        return '<{0} filter for {1}hz sample freq with {2} taps{3}>'.format(
+            self.filter_type,
+            self.sample_freq,
+            len(self.coefficients),
+            self.name and ': ' + self.name or ''
+        )
+
+    def _repr_html_(self):
+        self.plot_filter(prompt=False)
+
+
+__filter_db = shelve.open(os.path.join(os.path.dirname(__file__), 'filter_cache'))
+
+
+def filter_cache(method):
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        method_name = method.__name__
+        self, args = args[0], args[1:]
+        factory_args = sorted(self.__dict__.items())
+        key = pickle.dumps((method_name, factory_args, args, sorted(kwargs.items())))
+        cached_value = __filter_db.get(key)
+        if cached_value is None:
+            cached_value = method(self, *args, **kwargs)
+            __filter_db[key] = cached_value
+        return cached_value
+    return wrapper
+
+
+class FilterFactory(object):
+    sample_freq = None
+    filter_size = None
+
+    def __init__(self, sample_freq, filter_size):
+        self.sample_freq = sample_freq
+        self.filter_size = filter_size
+
+    @filter_cache
+    def allpass(self):
+        return Filter(
+            'allpass',
+            None,
+            self.sample_freq,
+            scipy.fftpack.ifft(numpy.repeat(1, self.filter_size))
+        )
+
+    @filter_cache
+    def remez_filter(self, inner_bands, band_coefficients, grid_density=256, name=None):
+        return Filter(
+            'remez',
+            name,
+            self.sample_freq,
+            scipy.signal.remez(
+                self.filter_size,
+                [0] + inner_bands + [self.sample_freq / 2],
+                band_coefficients,
+                Hz=self.sample_freq,
+                maxiter=200,
+                grid_density=256
+            ))
+
+    @filter_cache
+    def butter_filter(self, order, cutoffs, btype, name=None):
+        ba = scipy.signal.butter(order,
+                                 2 * numpy.pi * numpy.array(cutoffs),
+                                 btype=btype,
+                                 analog=True,
+                                 output='ba'
+                                 )
+
+        t = numpy.linspace(0, float(self.filter_size - 1) / self.sample_freq, self.filter_size)
+
+        coefs = scipy.signal.impulse(ba, T=t)[1]
+
+        coefs /= float(self.sample_freq)  # TODO - What sort of normalization do we need here?
+        return Filter(
+            'butterworth',
+            name,
+            self.sample_freq,
+            coefs
+        )
+
+    @filter_cache
+    def hilbert_filter(self, signal, name=None):
+        coefs = scipy.signal.hilbert(signal)
+        coefs /= coefs.sum()
+        return Filter(
+            'hilbert',
+            name,
+            self.sample_freq,
+            coefs
+        )
+
+    def close(self):
+        __filter_db.sync()
--- filter/runspeakers.py
+++ filter/runspeakers.py
@@ -1,136 +1,84 @@
-import sys
-import numpy
-import numpy as np
-import scipy.fftpack
-
-import scipy.signal
-from matplotlib import pyplot
+#!/usr/bin/env python
+from filter import FilterFactory
 
 
 def main():
-    """
-        scipy.signal.remez(
-            filter_size,
-            [0, cutoff_freq * 0.9,
-             cutoff_freq * 1.1, cutoff_freq_2 * 0.9,
-             cutoff_freq_2 * 1.1, sample_freq / 2],
-            [0, 1, 0],
-            Hz=sample_freq,
-            maxiter=50
-        ),
-        scipy.signal.remez(
-            filter_size,
-            [0, cutoff_freq_2 * 0.9, cutoff_freq_2 * 1.1, sample_freq / 2],
-            [0, 1],
-            Hz=sample_freq,
-            maxiter=50
-        ),
-        """
+    filter_factory = FilterFactory(sample_freq=44100, filter_size=1025)
 
-    filter_size = 1025
-    cutoff_freq = 310
-    cutoff_freq_2 = 3000
-    sample_freq = 44100
+    filters = build_remez_filters(filter_factory)
 
-    transition_width = 125
-    transition_width_2 = 100
-
-    print "Building filters..."
-
-    filters = [
-        scipy.signal.remez(
-            filter_size,
-            [0, cutoff_freq - transition_width,
-             cutoff_freq + transition_width, sample_freq / 2],
-            [1, 0],
-            Hz=sample_freq,
-            maxiter=200,
-            grid_density=256
-        ),
-        scipy.signal.remez(
-            filter_size,
-            [0, cutoff_freq - transition_width,
-             cutoff_freq + transition_width, cutoff_freq_2 - transition_width_2,
-             cutoff_freq_2 + transition_width_2, sample_freq / 2],
-            [0, 1, 0],
-            Hz=sample_freq,
-            maxiter=199,
-            grid_density=256
-        ),
-        scipy.signal.remez(
-            filter_size,
-            [0, cutoff_freq_2 - transition_width_2,
-             cutoff_freq_2 + transition_width_2, sample_freq / 2],
-            [0, 1],
-            Hz=sample_freq,
-            maxiter=201,
-            grid_density=256
-        ),
-    ]
-
-    filters = [scipy.fftpack.ifft(numpy.repeat(1, filter_size)), filters[0], filters[1]]
-
-    #filters = filters[:2]
-
-    #for filter_ in filters:
-    #    filter_[0] = filter_[-1] = 0
+    #filters = [filter_factory.allpass()] * 3
+    filters = build_iir_filters(filter_factory)
 
     try:
         for filter_ in filters:
-            plot_filter(filter_, sample_freq)
+            filter_.plot_filter()
     except:
         pass
 
     from filterlib import run_filter
 
     run_filter({
-        'filters': filters,
-        'sample_rate': sample_freq,
+        'filters': [f.coefficients for f in filters],
+        'sample_rate': filter_factory.sample_freq,
         'input_device': 3,
         'output_device': 11,
         'print_debug': True
     })
 
 
-def plot_filter(filter_coefs, sample_freq):
+def build_iir_filters(filter_factory):
+    cutoff_freq = 310
+    cutoff_freq_2 = 3000
 
-    #fig = pyplot.figure()
-    #ax1 = fig.add_subplot(111)
-    #freq, response = scipy.signal.freqz(filter_coefs)
+    return [
+        filter_factory.butter_filter(
+            4,
+            cutoff_freq,
+            btype='lowpass',
+            name='Lowpass at {0}'.format(cutoff_freq)
+        ),
+        filter_factory.butter_filter(
+            4,
+            [cutoff_freq, cutoff_freq_2],
+            btype='bandpass',
+            name="Bandpass for {0}hz-{1}hz".format(cutoff_freq, cutoff_freq_2)
+        ),
+        filter_factory.butter_filter(
+            4,
+            cutoff_freq_2,
+            btype='highpass',
+            name="Highpass for {0}hz".format(cutoff_freq_2)
+        )
+    ]
 
-    #ax1.semilogy(freq / (2 * np.pi), np.abs(response), 'b-')
-    #pyplot.show()
-    #return
-    t = numpy.linspace(0, float(len(filter_coefs - 1)) / sample_freq, len(filter_coefs))
-    fig = pyplot.figure(figsize=(10, 10))
 
-    pyplot.subplot(211)
-    pyplot.plot(t, filter_coefs)
-    pyplot.grid(True)
-    pyplot.xlabel('Time (s)')
-    pyplot.ylabel('Filter coefficient')
-    pyplot.title('Filter impulse response')
+def build_remez_filters(filter_factory):
+    cutoff_freq = 310
+    cutoff_freq_2 = 3000
 
-    ax1 = fig.add_subplot(212)
-    (freq, ampl) = scipy.signal.freqz(filter_coefs)
-    freq_hz = (freq * sample_freq / (2 * numpy.pi))[1:]
+    transition_width = 125
+    transition_width_2 = 100
 
-    pyplot.semilogx(freq_hz, 20 * numpy.log10(numpy.abs(ampl[1:])), 'b-')
-    pyplot.grid(True)
-    pyplot.ylabel('Amplitude (dB)')
-    pyplot.legend(['Amplitude', 'Phase'], loc='upper right')
-
-    ax1.twinx()
-    pyplot.semilogx(freq_hz, 180 / numpy.pi * numpy.unwrap(numpy.angle(ampl[1:])), 'r-')
-    pyplot.ylabel('Phase angle (deg)')
-    pyplot.xlabel('Frequency (Hz)')
-    pyplot.legend(['Phase'], loc='lower right')
-    pyplot.title('Filter frequency response')
-
-    print 'Please close figure window to continue'
-    pyplot.show()
+    return [
+        filter_factory.remez_filter(
+            [cutoff_freq - transition_width, cutoff_freq + transition_width],
+            [1, 0],
+            name='Lowpass at {0}hz'.format(cutoff_freq)
+        ),
+        filter_factory.remez_filter(
+            [cutoff_freq - transition_width, cutoff_freq + transition_width,
+             cutoff_freq_2 - transition_width_2, cutoff_freq_2 + transition_width_2],
+            [0, 1, 0],
+            name='Bandpass {0}hz-{1}hz'.format(cutoff_freq, cutoff_freq_2)
+        ),
+        filter_factory.remez_filter(
+            [cutoff_freq_2 - transition_width_2, cutoff_freq_2 + transition_width_2],
+            [0, 1],
+            name='Highpass at {0}hz'.format(cutoff_freq_2)
+        ),
+    ]
 
 
 if __name__ == '__main__':
     main()
-
